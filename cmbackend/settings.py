93bfde13ffabe07cedd4d57256a166c135ad2bea
saiMulagada <sai.star47@gmail.com>
2024-12-29 01:23:37 +0530

    fix code

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -36,7 +36,7 @@ cloudinary.config(
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
